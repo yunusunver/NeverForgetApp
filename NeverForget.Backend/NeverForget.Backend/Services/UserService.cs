4496f695f51fec2c4bfdd5a674276d6b5bdad59c
yunver <yunus.unver1995@gmail.com>
2019-12-14 00:12:01 +0300

    Crud operations completed for MongoDb

--- NeverForget.Backend/NeverForget.Backend/Services/UserService.cs
+++ NeverForget.Backend/NeverForget.Backend/Services/UserService.cs
@@ -17,9 +17,19 @@ namespace NeverForget.Backend.Services
 
 
         public List<User> Get()=> _users.Find(user=>true).ToList();
-
+  public User Create(User user){
+            _users.InsertOne(user);
+            return user;
+        }
         
+    public void Update(string id,User userIn)=>
+        _users.ReplaceOne(user=> user.id==id,userIn);
 
+    public void Remove(User userIn)=>
+        _users.DeleteOne(user=>user.id==userIn.id);
+
+    public void Remove(string id)=>
+        _users.DeleteOne(user=>user.id==id);
         
     }
 }
