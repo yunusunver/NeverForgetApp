3e002040437905091b068aa2e4d957fef9d4952d
yunusunver <yunus.unver1995@gmail.com>
2019-12-15 10:34:25 +0300

    Notes added in Models

---
+++ NeverForget.Backend/NeverForget.Backend/Models/Notes.cs
@@ -0,0 +1,16 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace NeverForget.Backend.Models
+{
+    public class Notes
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public ObjectId Id { get; set; }
+        public int categoryId { get; set; }
+        public string name { get; set; }
+        public string Description { get; set; }
+        public ObjectId userId { get; set; }
+    }
+}
